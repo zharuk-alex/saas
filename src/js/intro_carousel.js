128ba129b48995ccec2f8bf8d7e7595e70f4d0cb
zzharuk <zzharuk@gmail.com>
2018-09-25 22:50:33 +0300

    proto

--- src/js/intro_carousel.js
+++ src/js/intro_carousel.js
@@ -4,9 +4,16 @@ $(function () {
     var last_element = $(this).find('.carousel-item').last();
     var last_element_heading = $(this).data('heading');
     if(carousel_items == (e.to+1)){
-        last_element.delay( 1500 ).animate({
+        last_element.find('h1').delay( 1500 ).animate({
           opacity: 0
-        }, 300)
+        }, 600, function () {
+          console.log($(this));
+          $(this).text(last_element_heading).animate({
+            opacity: 1
+          }, 600).css({
+            color:'#f89f35'
+          })
+        })
     }
     console.log(carousel_items +" "+ (e.to+1));
     // console.log(last_element.text());
